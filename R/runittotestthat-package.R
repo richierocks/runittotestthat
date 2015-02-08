0a7f7d22b620b1f02d564956c1344230bcd0ce3e
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-02-08 13:03:05 +0300

    Typo and markup fixes

--- R/runittotestthat-package.R
+++ R/runittotestthat-package.R
@@ -5,18 +5,20 @@
 #' existing \code{RUnit} tests to \code{testthat} tests.
 #' 
 #' There are three functions of interest:
-#' 1. \code{\link{convert_test}} converts an individual \code{RUnit} test to a
-#' call to a \code{testthat} test.
-#' 2. \code{\link{convert_test_file}} sources all the tests in a file, converts 
-#' each one, then outputs them to a file (or file connection; 
+#' \enumerate{
+#' \item \code{\link{convert_test}} converts an individual \code{RUnit} test to
+#' a call to a \code{testthat} test.
+#' \item  \code{\link{convert_test_file}} sources all the tests in a file,  
+#' converts each one, then outputs them to a file (or file connection; 
 #' \code{\link[base]{stdout}} by default).
-#' \code{\link{convert_package_tests}} takes a string naming a package, or a 
-#' \code{devtools::package} object and loops over the files in `inst/tests` (or
-#' wherever you specify), converting the tests, and outputting to files or 
+#' \item \code{\link{convert_package_tests}} takes a string naming a package, or 
+#' a \code{devtools::package} object and loops over the files in `inst/tests` 
+#' (or wherever you specify), converting the tests, and outputting to files or 
 #' \code{\link[base]{stdout}}.
+#' }
 #' 
 #' @docType package
-#' @name regex
-#' @aliases regex regex-package
+#' @name runittotestthat
+#' @aliases runittotestthat runittotestthat-package
 #' @keywords utilities
 NULL
