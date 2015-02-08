7a9e058efc0bf4871d0bf8d023e5b179cf2c7b1b
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-02-08 12:51:13 +0300

    Added package intro help file.

---
+++ R/runittotestthat-package.R
@@ -0,0 +1,22 @@
+#' Convert RUnit tests to testthat tests.
+#' 
+#' \code{testthat} has some nice features like test caching that aren't
+#' suppported by \code{RUnit}.  This package lets you automatically convert your
+#' existing \code{RUnit} tests to \code{testthat} tests.
+#' 
+#' There are three functions of interest:
+#' 1. \code{\link{convert_test}} converts an individual \code{RUnit} test to a
+#' call to a \code{testthat} test.
+#' 2. \code{\link{convert_test_file}} sources all the tests in a file, converts 
+#' each one, then outputs them to a file (or file connection; 
+#' \code{\link[base]{stdout}} by default).
+#' \code{\link{convert_package_tests}} takes a string naming a package, or a 
+#' \code{devtools::package} object and loops over the files in `inst/tests` (or
+#' wherever you specify), converting the tests, and outputting to files or 
+#' \code{\link[base]{stdout}}.
+#' 
+#' @docType package
+#' @name regex
+#' @aliases regex regex-package
+#' @keywords utilities
+NULL
